2cf8bac3eb05aff07b8850274d78ba09da9ccc95
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-21 16:53:42 -0400

    Add logic to generate grid, vertical walls, and horizontal walls.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import './scss/index.scss';
 import { Engine, Render, Runner, World, Bodies } from 'matter-js';
 
 // Constants
-const width = 600;
-const height = 600;
+const width: number = 600;
+const height: number = 600;
+const wallWidth: number = 40;
 
 // Create engine and world object.
 const engine = Engine.create();
@@ -37,14 +38,39 @@ Runner.run(Runner.create(), engine);
 // Wall objects stored in an array.
 const walls = [
 	// Top Wall
-	Bodies.rectangle(width / 2, 0, width, 25, { isStatic: true }),
+	Bodies.rectangle(width / 2, 0, width, wallWidth, { isStatic: true }),
 	// Bottom Wall
-	Bodies.rectangle(width / 2, height, width, 25, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, wallWidth, { isStatic: true }),
 	// Left Wall
-	Bodies.rectangle(0, height / 2, 25, height, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, wallWidth, height, { isStatic: true }),
 	// Right Wall
-	Bodies.rectangle(width, height / 2, 25, height, { isStatic: true })
+	Bodies.rectangle(width, height / 2, wallWidth, height, { isStatic: true })
 ];
 
 // Add walls to world
 World.add(world, walls);
+
+/*
+ --MAZE GENERATION--
+ Grid is a 2d array with all values initialized to false. After visiting 
+ a cell on the grid, the value will change to true. We pick a random starting point
+ and randomly pick a neighbor to visit.
+*/
+// Init a 3x3 Grid with every value initialized to false.
+/*
+  Steps
+  1. Init an array of size 3,
+  2. Fill array with null values,
+  3. For each null value, return an array of length 3 with false values.
+  4. Create 2d array to track horizontal and vertical walls.
+*/
+const grid: boolean[][] = Array(3).fill(null).map(() => Array(3).fill(false));
+
+// Init verticals array.
+const verticals = Array(3).fill(null).map(() => Array(2).fill(false));
+// Init horizontals array.
+const horizontals = Array(2).fill(null).map(() => Array(3).fill(false));
+
+console.log(grid);
+console.log(verticals);
+console.log(horizontals);
