c6f72a843c0a0b62a41ad412c56ded8282db2029
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-21 16:25:14 -0400

    Refactor Wall logic to dynamically generate the wall dimensions.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import './scss/index.scss';
 
-import {
-	Engine,
-	Render,
-	Runner,
-	World,
-	Bodies,
-	MouseConstraint,
-	Mouse
-} from 'matter-js';
+import { Engine, Render, Runner, World, Bodies } from 'matter-js';
 
 // Constants
-const width = 800;
+const width = 600;
 const height = 600;
 
 // Create engine and world object.
@@ -35,44 +27,24 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 // Add mouse functionality to the world. Allows user to drag shapes on the canvas.
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse: Mouse.create(render.canvas)
-	})
-);
+// World.add(
+// 	world,
+// 	MouseConstraint.create(engine, {
+// 		mouse: Mouse.create(render.canvas)
+// 	})
+// );
 
 // Wall objects stored in an array.
 const walls = [
-	Bodies.rectangle(400, 0, 800, 25, { isStatic: true }),
-	Bodies.rectangle(400, 600, 800, 25, { isStatic: true }),
-	Bodies.rectangle(0, 400, 25, 800, { isStatic: true }),
-	Bodies.rectangle(800, 400, 25, 800, { isStatic: true })
+	// Top Wall
+	Bodies.rectangle(width / 2, 0, width, 25, { isStatic: true }),
+	// Bottom Wall
+	Bodies.rectangle(width / 2, height, width, 25, { isStatic: true }),
+	// Left Wall
+	Bodies.rectangle(0, height / 2, 25, height, { isStatic: true }),
+	// Right Wall
+	Bodies.rectangle(width, height / 2, 25, height, { isStatic: true })
 ];
 
 // Add walls to world
 World.add(world, walls);
-
-// Generate Random Shapes
-for (let i = 0; i < 30; i++) {
-	if (Math.random() < 0.3) {
-		World.add(
-			world,
-			Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-		);
-	} else if (Math.random() < 0.7) {
-		World.add(
-			world,
-			Bodies.trapezoid(Math.random() * width, Math.random() * height, 50, 50, 1)
-		);
-	} else {
-		World.add(
-			world,
-			Bodies.circle(Math.random() * width, Math.random() * height, 50, {
-				render: {
-					fillStyle: 'red'
-				}
-			})
-		);
-	}
-}
