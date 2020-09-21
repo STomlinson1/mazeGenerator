14c2e10b9dccd9c73ff4e2fd1ddfd444f953bcb2
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-21 15:18:51 -0400

    Import Matter.js and setup config.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import './scss/index.scss';
 
-console.log('Hello World');
+import { Engine, Render, Runner, World, Bodies } from 'matter-js';
+
+// Create engine and world object.
+const engine = Engine.create();
+const { world } = engine;
+
+// Create render object to show content on the screen.
+const render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+		width: 800,
+		height: 600
+	}
+});
+
+// Draw Content to screen.
+Render.run(render);
+Runner.run(Runner.create(), engine);
